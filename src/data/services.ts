821c10a5d90a2ace82395ed470ccd52b3f4f1b2d
vlohi <veikko.lohi@gmail.com>
2026-01-28 09:32:58 +0200

    Create services.ts

---
+++ src/data/services.ts
@@ -0,0 +1,45 @@
+export const services = [
+  {
+    id: "prosessit",
+    title: "Prosessien kehitys",
+    short: "Sujuva toiminta on sekä tehokasta että laadukasta.",
+    long: `
+Kun prosessit ja niiden eri vaiheet toimivat saumattomasti yhteen, on toiminta sujuvaa ja tehokasta. Virheitä syntyy vähemmän ja työnteko on vähemmän turhauttavaa, suorastaan mielekästä.
+Tein diplomityöni prosessien kehittämisestä ja laadusta vuosina 2005-2006. Se yhdisti molemmat suuntautumisalani opinnoissani: tuotantotalouden sekä automaation tietotekniikan. Sillä tiellä jatkan edelleen.
+Yleinen kokemukseni on, että kun laatua parannetaan prosesseja kehittämällä, paranee laadun kanssa käsi kädessä myös tuottavuus ja työntekijöiden tyytyväisyys. Tätä laadukasta tehokkuutta tuon mielelläni teillekin.
+    `,
+  },
+    {
+    id: "laatu",
+    title: "Laadun parantaminen",
+    short: "Laatu syntyy toimivista prosesseista.",
+    long: `
+Laadukasta tulosta syntyy, kun prosessin kaikki vaiheet ja osa-alueet toimivat saumattomasti yhteen. Vahvan testauspuolen kokemukseni avulla autan teitä testaamaan optimaalisesti. Riittävän hyvin, jotta tiedetään, onko tuote riittävän hyvä. Kuitenkin niin, että testauskustannukset eivät karkaa pilviin.
+Testaus ei silti itsessään paranna laatua. Se vain kertoo nykytilanteen. Laatu syntyy paitsi vikoja korjaamalla, niin vielä tehokkaammin sillä, että vältetään niiden vikojen tuottamista paremmalla kehitys- tai tuotantoprosessilla. Siksi laatua parannettaessa käsitellään aina koko prosessia, ei pelkkää testausvaihetta.
+    `,
+  },
+    {
+    id: "prosessikuvaus",
+    title: "Prosessien kuvaus",
+    short: "Avaa silmiä niin vanhoille kuin uusille työntekijöille.",
+    long: `
+Selkeä prosessikaavio ja sen sanallinen kuvaus ovat erinomainen lähtökohta toiminnan kehittämiselle. Sillä saadaan:
+a)	Tilannekuva yhtenäiseksi. On hyvin yleistä, että samassakin organisaatiossa eri henkilöillä on erilainen käsitys siitä, miten kokonaisuus toimii. Prosessikaavio ja -kuvaus selkeyttävät tilannetta ja antavat saman käsityksen kaikille. Toisinaan tässä vaiheessa tulee esille, että tiimissä on erilaisia tapoja toimia tietyissä tilanteissa. Tällöin on hyvä tilaisuus keskustella, pitäisikö kaikkien toimia samalla tavalla, vai onko prosessiin hyvä jättää tilaa vaihtoehtoisille tavoille.
+b)	Turhat välivaiheet näkyviksi. Usein prosessikuvausta tehdessä huomataan, että prosessissa on vaiheita, jotka eivät tuota mitään lisäarvoa, tai jotka pystyttäisiin tekemään paremmin tai vähemmällä vaivalla jossain toisessa vaiheessa.
+c)	Hyvä pohja kehittämiselle. Prosessimuutoksia on helpompi suunnitella ja viestiä prosessikaavion kanssa kuin ilman sellaista.
+d)	Selkeyttävää materiaalia perehdytykseen. Prosessikaaviosta ja -kuvauksesta uudet työntekijät näkevät, mikä on oman osuuden suhde kokonaisuuteen. Se nopeuttaa oppimista ja mahdollistaa järkevän toiminnan sellaisissakin tilanteissa, jotka tulevat työntekijälle esiin ensimmäistä kertaa.
+Voin perehtyä prosesseihinne ja tehdä tarvittavat prosessikaaviot ja kuvaukset. Tai sitten voin auttaa teitä tekemään niitä itse ja tarvittaessa katselmoida yhdessä lopputulokset.
+    `,
+  },
+    {
+    id: "jalkautus",
+    title: "Muutosten jalkauttaminen",
+    short: "Sillä on merkitystä, miten asia viedään käytäntöön.",
+    long: `
+Hyvätkään kehitysideat eivät aina auta, jos väki ei toimi niiden mukaisesti. Autan mielelläni miettimään yhdessä sitä, miten eri roolien toimijat ”ostavat” uudet käytännöt ja saavat niistä täyden potentiaalin irti. Keinot riippuvat aina tilanteesta, niin kehitysidean laadusta kuin organisaation nykytilasta, kulttuurista, ihmisistä ja heidän osaamisistaan jne.
+Tätä asiaa kannattaa yleensä miettiä jo kehittämisprojektin alkuvaiheessa, sillä ihmisten osallistaminen alusta asti auttaa usein paitsi sopivampien ideoiden löytymiseen, niin myös heidän innokkuuteensa ottaa niitä käyttöön.
+Osallistaminen ei kuitenkaan saa olla päälle liimattua tai näennäistä, vaan sellaista, jossa työntekijöillä on todellinen mahdollisuus vaikuttaa ja heitä oikeasti kuunnellaan. Tämä toteutuu esimerkiksi halukkaiden tai valittujen työntekijöiden yksilöhaastatteluilla, joita olen tehnyt useammassa organisaatiossa. Tai monella muulla mahdollisella tavalla.
+Uusien käytäntöjen omaksumista voidaan tukea myös monella teknisellä ja viestinnällisellä keinolla, joista useimmat eivät maksa juuri mitään. Mutta säästävät paljon pitkällä tähtäimellä.
+    `,
+  }
+];
